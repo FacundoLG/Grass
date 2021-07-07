f250320bf3f5780a25b099aaf3b00759415de182
FacundoLG <facu-judoca@hotmail.com.ar>
2021-07-07 10:40:49 -0300

    cards sistem and get

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,29 +1,56 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../styles/notes.css'
 import Input from './Input'
 import NoteForm from './NoteForm'
 const Notes = () => {
-    const [form,setForm] = useState(false)
-    const [inputTitle,setInputTitle] = useState("")
-    const newTask = (title) =>{
+    const [form, setForm] = useState(false)
+    const [inputTitle, setInputTitle] = useState('')
+    const [notes, setNotes] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/api')
+            .then((data) => data.json())
+            .then((data) => setNotes(data))
+    }, [])
+
+    const newTask = (title) => {
         setInputTitle(title)
         console.log(inputTitle)
         setForm(true)
     }
-    const addTask = (data) =>{
+
+    const addTask = (data) => {
         console.log(data)
         setForm(false)
     }
     return (
-            <div className="Notes">
-                {form? <NoteForm title={inputTitle} ret={(data) => {addTask(data)}}/>: ""}
-                <div>
-                {form? "": <Input note={true} placeholder={"note title..."} ret={(title)=>{ newTask(title)}}/>}
-                </div>
-                <div>
-
-                </div>
+        <div className="Notes">
+            {form ? (
+                <NoteForm
+                    title={inputTitle}
+                    ret={(data) => {
+                        addTask(data)
+                    }}
+                />
+            ) : (
+                ''
+            )}
+            <Input
+                note={true}
+                placeholder={'note title...'}
+                ret={(title) => {
+                    newTask(title)
+                }}
+            />
+            <div className="noteContainer">
+                {notes.map((note) => (
+                    <div className="note">
+                        <h5>{note.title}</h5>
+                        <p>{note.content}</p>
+                    </div>
+                ))}
             </div>
+        </div>
     )
 }
 
